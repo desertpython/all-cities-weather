601259daa61c3537ff0a93bf2f52854e325b7a7f
Desert Python <mattsenneff@aol.com>
2020-07-05 15:55:37 -0700

    more pseudo and updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var myCityArray = [];
 
 $(".search_icon").on("click",function(){
     var cityData = $(".search_input").val()
@@ -18,11 +19,31 @@ $(".search_icon").on("click",function(){
         console.log(data.main.humidity)
 
         pressure.innerHTML = data.main.pressure
-        console.log(data.main.pressure)
+        console.log(data.main.pressure);
+        console.log(data);
+        console.log("lat ", data.coord);
+        // get uv index
+        getUVindex(data.coord.lon,data.coord.lat );
+        //empty element
+        cityArray()
 
-    //fetch(http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}).then(repsonseii => repsonseii.json())
-    //.then(dataii => {console.log(dataii)})
-    
+        function cityArray(){
+        myCityArray.push(cityData)
+        parentList.innerHTML = " "
+        for(i=0;i<myCityArray.length;i++){
+            var currentCity = myCityArray[i];
+            localStorage.setItem('cityKey',myCityArray)
+            var parentList = document.querySelector(".list-group")
+            var myLi = document.createElement("li")
+            myLi.textContent = currentCity
+            parentList.append(myLi)
+            
+            //need to retrieve item from local storage
+            //but solve extra appends in loop
+
+        }
+    }
+        
     
     
     
@@ -41,5 +62,18 @@ $(".search_icon").on("click",function(){
 
 })
 
+function getUVindex(lon, lat){
+    fetch("http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}).then(repsonseii => repsonseii.json()")
+    .then(dataii => {console.log(dataii)})
+}
+
 
 })
+
+// var date = //moment current date
+// var dateOne = //moment day 1 0f 5 for forecast
+// var dateTwo = //moment day 2 0f 5 for forecast
+// var dateThree = //moment day 3 0f 5 for forecast
+// var dateFour = //moment day 4 0f 5 for forecast
+// var dateFive = //moment day 5 0f 5 for forecast
+
