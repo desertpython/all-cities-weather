cac8dab3887327a7ef61eaadf3339db284f0657d
Desert Python <mattsenneff@aol.com>
2020-07-18 13:47:15 -0700

    session storage

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var myCityArray = [];
 
 $(".search_icon").on("click",function(){
     var cityData = $(".search_input").val()
@@ -8,7 +7,7 @@ $(".search_icon").on("click",function(){
     var lat = cityData.lat
     var lon = cityData.lon
     console.log(cityData);
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityData + "&units=imperial&appid&appid=6bc6be181023384e301d45eee4f5659e")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityData + "&units=imperial&appid&appid=6c5343dfc16a8257dd6fc39f97ca3f8a")
     .then(response => response.json())
     .then(data => {console.log(data.main)
         cardText.innerHTML = "Temperarture is " +  data.main.temp + " F"
@@ -21,33 +20,31 @@ $(".search_icon").on("click",function(){
         windSpeed.innerHTML = " The current wind speed is " + data.wind.speed + " mph"
         console.log(data.wind.speed);
 
-        localStorage.setItem("city",cityData);
-
     
         console.log("lat ", data.coord);
         // get uv index
         getUVindex(data.coord.lon,data.coord.lat );
         //empty elementS
         
-        cityArray()
+        
+        var myCityArray = sessionStorage.getItem("savedCityNames");
 
-
-        function cityArray(){
+        if (myCityArray){
+            myCityArray = myCityArray.split(",");
+    
         myCityArray.push(cityData)
-        parentList.innerHTML = ""
-        for(i=0;i<myCityArray.length;i++){
-            var currentCity = myCityArray[i];
-            localStorage.setItem('cityKey',myCityArray)
-            var parentList = document.querySelector(".list-group")
-            var myLi = document.createElement("li")
-            myLi.textContent = currentCity
-            parentList.append(myLi)
-            
-            //need to retrieve item from local storage
-            //but solve extra appends in loop
-
+        myCityArray = myCityArray.join(",");
         }
-    }
+        else {
+            myCityArray = cityData;
+        }
+        sessionStorage.setItem("savedCityNames", myCityArray);
+        
+        var parentList = document.querySelector(".list-group")
+        var myLii = document.createElement("li")
+        myLii.textContent = cityData;
+        parentList.append(myLii)
+    });
         
     
     
@@ -64,20 +61,34 @@ $(".search_icon").on("click",function(){
     
 
    
-
+    
 })
+var localDisplay = sessionStorage.getItem("savedCityNames");
+if (localDisplay) {
+    var localSplit = localDisplay.split(",");
+
+for(i=0; i<localSplit.length; i++){
+    var currentCity = localSplit[i];
+    console.log(currentCity);
+    var parentList = document.querySelector(".list-group")
+    //parentList.innerHTML = currentCity;
+    var myLi = document.createElement("li")
+    myLi.textContent = currentCity
+    parentList.append(myLi)
+    
+    //need to retrieve item from local storage
+    //but solve extra appends in loop
+
+}}
 
 function getUVindex(lon, lat){
-    fetch("https://api.openweathermap.org/data/2.5/uvi?appid=&lat"+ lat + lon)
+    fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=6d095275f4b36a42a20e3423879e63af")
     .then(repsonseii => repsonseii.json()
     .then(dataii => {console.log(dataii)}));
 
-}
-})
+};
 
-// var date = //moment current date
-// var dateOne = //moment day 1 0f 5 for forecast
-// var dateTwo = //moment day 2 0f 5 for forecast
-// var dateThree = //moment day 3 0f 5 for forecast
-// var dateFour = //moment day 4 0f 5 for forecast
-// var dateFive = //moment day 5 0f 5 for forecast
+// 1. grab search input value "atlanta"
+// 2. set local storage
+// 3. grab local storage
+// 4. Display on page (append?)
